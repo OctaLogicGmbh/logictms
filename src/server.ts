7e8eb284cda7d76602dee0859969a1ea1e3c32c0
OctaLogicGmbh <124939908+OctaLogicGmbh@users.noreply.github.com>
2024-07-09 16:35:57 -0600

    sin amplify.yml

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import 'dotenv/config';
-
 import { Hono } from 'hono';
 import { readFile } from 'node:fs/promises';
-
 import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
-
 import { setUserCookies } from './api/auth/helpers';
 import api from './api/server';
 import { decodeBase64 } from './utils/common';
@@ -24,7 +21,7 @@ if (!isProd) {
       window.__vite_plugin_react_preamble_installed__ = true
     </script> 
     <script type="module" src="/@vite/client"></script>
-`;
+  `;
 
   html = html.replace('<head>', reactRefresh);
 }
@@ -32,6 +29,7 @@ if (!isProd) {
 const app = new Hono()
   .use('*', async (c, next) => {
     c.res.headers.set('X-Powered-By', 'Hono');
+    console.log('Setting X-Powered-By header to Hono');
 
     const accessToken = c.req.query('access_token');
 
@@ -56,7 +54,10 @@ const app = new Hono()
   .route('/api', api)
   //.use('/static/*', serveStatic({ root: isProd ? './build/' : './' }))
   .use('/assets/*', serveStatic({ root: isProd ? './build/' : './' }))
-  .get('/*', (c) => c.html(html));
+  .get('/*', (c) => {
+    console.log('Serving HTML content');
+    return c.html(html);
+  });
 
 export type AppType = typeof app;
 export default app;
