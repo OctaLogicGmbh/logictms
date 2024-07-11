459447db1c81fc6ad21ce77bce06f2ffc24500f0
OctaLogicGmbh <124939908+OctaLogicGmbh@users.noreply.github.com>
2024-07-10 18:56:29 -0600

    amplify22

--- fix-paths.js
+++ fix-paths.js
@@ -3,10 +3,11 @@ import path from 'path';
 
 async function fixServerPath() {
   const serverPath = path.resolve('dist/src/server.js');
+  const targetPath = path.resolve('dist/src/server.js'); // Ajusta esta línea según tu estructura
 
   try {
     await fs.access(serverPath);
-    console.log('Server file path is correct.');
+    console.log(`Server file not found: ${serverPath} ${targetPath} `);
   } catch (err) {
     console.error(`Server file not found: ${serverPath}`, err);
   }
